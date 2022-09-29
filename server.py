091fab276249bf0af0ba941a2f3b347e36c162ae
Shoter99 <shoter998@gmail.com>
2022-09-29 12:44:58 +0200

    first commit

---
+++ server.py
@@ -0,0 +1,44 @@
+import socket, threading
+
+host = '127.0.0.1'
+port = 7976
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((host,port))
+server.listen()
+
+clients = []
+nicknames = []
+
+def broadcast(msg):
+    for client in clients:
+        client.send(msg)
+
+def handle(client):
+    while True:
+        try:
+            msg = client.recv(1024)
+            broadcast(msg)
+        except:
+            index = clients.index(client)
+            clients.remove(client)
+            client.close()
+            nickname = nicknames[index]
+            broadcast('{} left!'.format(nickname).encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        client, address = server.accept()
+        print('Connected with {}'.format(str(address)))
+        client.send('NICKNAME'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        print("Nickname is {}".format(nickname))
+        broadcast("{} joined!".format(nickname).encode('ascii'))
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+receive()
